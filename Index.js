ecc31a4762f81794fbe23effad43fb954da0100a
guaxininho <sammydevjs@gmail.com>
2023-05-11 19:03:49 -0300

    primeiro dia, visual feito, falta formulario e javascript

--- Index.js
+++ Index.js
@@ -1,1 +1,10 @@
 // Aqui farei a CalculadadoraIMC
+const imcbutton = document.querySelector("#imcbutton");
+
+const teclas = document.querySelector(".teclas");
+
+imcbutton.addEventListener("click", () => {
+  if (teclas.classList[0] === "teclas") {
+    // teclas.classList[0] = "formulario"; aqui ta errado
+  }
+});
